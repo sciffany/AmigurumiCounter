5c5deadc4a5f6c6b35f12516d838f3629689b850
Tiffany Chong <sciffany@gmail.com>
2023-02-25 11:30:42 +0800

    Add storage functions

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { memo, useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 enum StitchNumber {
   nothing,
@@ -24,125 +25,171 @@ type Stitch = {
 type StitchRow = Stitch[];
 
 export function Main() {
+  const [pattern, setPattern] = useState<number[]>([]);
 
-  const [pattern, setStitches] = useState<StitchRow[]>([[]]);
+  async function addRow() {
+    const previousRow = await AsyncStorage.getItem(`@amigurumi/${pattern.length-1}`);
 
-  function addStitch(rowNumber: number) {
-    const newPattern = [...pattern];
-    newPattern[rowNumber].push({
-      stitchType: StitchType.single,
-      stitchNumber: StitchNumber._1,
-    });
-    setStitches(newPattern);
-  }
-  47
-  function removeStitch(rowNumber: number) {
-    const newPattern = [...pattern];
-    newPattern[rowNumber].pop();
-    setStitches(newPattern);
-  }
+    if (previousRow) {
+      const prevRowJson = JSON.parse(previousRow) as Stitch[];
+      const effectiveLength = getEffectiveLength(prevRowJson);
+      setPattern(pattern => [...pattern, effectiveLength]);
 
-  function addRow() {
-    const newPattern = [...pattern];
-    const lastRow = pattern[pattern.length-1];
-    const effectiveLength = getEffectiveLength(lastRow)
-    newPattern.push(Array(effectiveLength).fill({stitchType: StitchType.single, stitchNumber: StitchNumber.nothing} as Stitch));
-    setStitches(newPattern);
-  }
+      await AsyncStorage.setItem(
+        `@amigurumi/${pattern.length}`,
+        JSON.stringify(Array(effectiveLength).fill(
+          [
+            {
+              stitchType: StitchType.single,
+              stitchNumber: StitchNumber.nothing,
+            }
+          ]
+        ))
+      );
 
-  function deleteRow() {
-    if (pattern.length > 1) {
-      const newPattern = [...pattern];
-      newPattern.pop();
-      setStitches(newPattern);
+    } else {
+      setPattern(pattern => [...pattern, 0]);
     }
   }
 
-  function changeStitchType(rowNumber: number, stitchIndex: number) {
-    setStitches((pattern: StitchRow[]) => {
-      const newPattern = [...pattern];
-      const newStitch = {...newPattern[rowNumber][stitchIndex]};
+  async function removeRow() {
+    setPattern(pattern => pattern.slice(0, pattern.length-1));
+    try {
+      await AsyncStorage.removeItem(`@amigurumi/${pattern.length-1}`)
+    } catch (e) {
+    }
+  }
+
+  return (
+    <View className='px-6 py-12'>
+      <View className="flex flex-row items-center justify-center">
+        <Text className="text-lg">Amigurumi Counter App</Text>
+      </View>
+      <ScrollView>
+        {
+          pattern.map((rowCount, rowIndex) => {
+            return <StitchRow key={Math.random()} rowIndex={rowIndex}/>
+          })
+        }
+      </ScrollView>
+      <View className="flex flex-row justify-center">
+        <TouchableOpacity  className="bg-blue-500 rounded-full flex w-24 h-12 flex-row justify-center items-center">  
+          <Text onPress={addRow} className="text-white">+ Add Row</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="bg-red-500 rounded-full flex w-24 h-12 flex-row justify-center items-center">
+          <Text onPress={removeRow} className="text-white">- Delete Row</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+  
+};
+
+function StitchRow({rowIndex}: {rowIndex: number}) {
+  const [stitchRow, setStitchRow] = useState<Stitch[]>([]);
+  const getStitchRowFromStorage = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem(`@amigurumi/${rowIndex}`)
+      return jsonValue != null ? JSON.parse(jsonValue) as Stitch[]: [];
+    } catch(e) {
+      return null;
+    }
+  }
+
+  useEffect(()=>{
+    getStitchRowFromStorage().then((stitchRow)=>{
+      if (stitchRow && stitchRow.length > 0) {
+        setStitchRow(stitchRow);
+      }
+    });
+  },[rowIndex])
+
+  async function addStitch() {
+    setStitchRow(stitchRow=>
+      [...stitchRow, {
+        stitchType: StitchType.single,
+        stitchNumber: StitchNumber._1,
+      }]);
+    await storeData([...stitchRow, {
+      stitchType: StitchType.single,
+      stitchNumber: StitchNumber._1,
+    }]);
+  }
+  
+  async function removeStitch() {
+    setStitchRow(stitchRow => stitchRow.slice(0, stitchRow.length-1));
+    await storeData(stitchRow.slice(0, stitchRow.length-1));
+  }
+
+  const storeData = async (stitchRow: Stitch[]) => {
+    try {
+      const jsonValue = JSON.stringify(stitchRow)
+      await AsyncStorage.setItem(`@amigurumi/${rowIndex}`, jsonValue)
+    } catch (e) {
+      // saving error
+    }
+  }
+
+  async function changeStitchType(stitchIndex: number) {
+    const updateStitchType =  (stitchRow: StitchRow) => {
+      const newStitchRow = [...stitchRow];
+      const newStitch = {...stitchRow[stitchIndex]};
       newStitch.stitchNumber += 1;
       if (newStitch.stitchNumber === 4) {
         newStitch.stitchNumber = 0;
       }
-      newPattern[rowNumber][stitchIndex] = newStitch;
+      if (Number.isNaN(newStitch.stitchNumber)) {
+        newStitch.stitchNumber = 1;
+      }
 
-      if (newPattern[rowNumber][stitchIndex+1]) {
+      if (newStitchRow[stitchIndex+1]) {
         if (newStitch.stitchNumber === StitchNumber._2tog) {
-          newPattern[rowNumber][stitchIndex+1] = {
+          newStitchRow[stitchIndex+1] = {
             stitchType: newStitch.stitchType,
             stitchNumber: StitchNumber._2tog,
           };
-        } else if (newPattern[rowNumber][stitchIndex+1].stitchNumber === StitchNumber._2tog){
-          newPattern[rowNumber][stitchIndex+1] = {
+        } else if (newStitchRow[stitchIndex+1].stitchNumber === StitchNumber._2tog){
+          newStitchRow[stitchIndex+1] = {
             stitchType: newStitch.stitchType,
             stitchNumber: StitchNumber.nothing,
           };
         }
       }
-      return newPattern;
-    });
+
+      newStitchRow[stitchIndex] = newStitch;
+      return newStitchRow;
+    }
+    setStitchRow(updateStitchType);
+    await storeData(updateStitchType(stitchRow));
   }
 
-  return (
-    <View className='px-6 py-12'>
-      <Text className="text-lg">Amigurumi Counter App</Text>
-      <ScrollView>
-        {pattern.map((row, rowNumber) => <>
-          {rowNumber === 0 ?
-            <View className="flex flex-row justify-between items-center w-full border-y-2 border-gray-300 py-2">
-              <View className="flex flex-row flex-wrap justify-start flex-1">
-                {row.map((stitch, stitchIndex) => {
-                  return <StitchComponent key={Math.random()} rowNumber={rowNumber} stitch={stitch} stitchIndex={stitchIndex} setStitches={setStitches}/>;
-                })}
-              </View>
-              <View className="flex flex-col">
-                <AddStitchButton addStitch={addStitch} rowNumber={rowNumber} text="+"/>
-                <AddStitchButton addStitch={removeStitch} rowNumber={rowNumber} text="-"/>
-              </View>
-              <Text className="w-1/6 text-3xl">{getEffectiveLength(row)}</Text>
-            </View>
-            :
-            <View key={Math.random()} className="flex flex-row justify-between items-center w-full border-y-2 border-gray-300 py-2">
-              <View className="flex flex-row flex-wrap w-4/5">
-                {row.map((stitch, stitchIndex) => {
-                  return <StitchComponent key={Math.random()} rowNumber={rowNumber} stitch={stitch} stitchIndex={stitchIndex} changeStitchType={changeStitchType}/>;
-                })}
-              </View>
-              <Text className="w-1/6 text-3xl">{getEffectiveLength(row)}</Text>
-            </View>
-            }
-          </>
-        )}
-      </ScrollView>
-      <View className="flex flex-row justify-between">
-        <TouchableOpacity onPress={addRow} className="bg-blue-500 rounded-full flex w-24 h-12 flex-row justify-center items-center">
-          <Text className="text-white">+ Add Row</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={deleteRow} className="bg-red-500 rounded-full flex w-24 h-12 flex-row justify-center items-center">
-          <Text className="text-white">- Delete Row</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-};
+  return <View className="flex flex-row justify-between items-center w-full border-b-2 border-gray-300 py-2">
+          <View className="flex flex-row flex-wrap justify-start flex-1">
+            {stitchRow.map((stitch, stitchIndex)=>{
+              return <StitchComponent key={Math.random()} stitchIndex={stitchIndex} stitch={stitch} changeStitchType={changeStitchType}/>;
+            })}
+          </View>
+          <View className="flex flex-col">
+            <AddStitchButton addStitch={addStitch} text="+"/>
+            <AddStitchButton addStitch={removeStitch} text="-"/>
+          </View>
+          <Text className="w-1/6 text-3xl">{getEffectiveLength(stitchRow)}</Text>
+        </View>
+}
 
-function StitchComponent({stitch, stitchIndex, rowNumber, changeStitchType}: {stitch: Stitch, stitchIndex: number, rowNumber: number, changeStitchType: Function}) {
-
-  return <TouchableOpacity onPress={()=>{changeStitchType(rowNumber, stitchIndex)}} className="flex flex-row">
-    <View className={`h-8 w-8 ${getColourForStitchNumber(stitch.stitchNumber)} rounded-full flex flex-row justify-center items-center`}>
-      <Text className="text-white">{stitchNumberToString(stitch.stitchNumber)}</Text>
-    </View>
+function StitchComponent({changeStitchType, stitchIndex, stitch}: {changeStitchType:(stitchIndex: number) => void, stitchIndex: number, stitch: Stitch}) {
+  return <TouchableOpacity
+            onPress={()=>changeStitchType(stitchIndex)}
+            className={`flex flex-row justify-center items-center w-8 h-8 rounded-lg ${getColourForStitchNumber(stitch.stitchNumber)}`}>
+    <Text className="text-xl text-white">{stitchNumberToString(stitch.stitchNumber)}</Text>
   </TouchableOpacity>
 }
 
-function AddStitchButton({addStitch, rowNumber, text}: {
-  addStitch: (rowNumber: number) => void;
-  rowNumber: number;
+function AddStitchButton({addStitch, text}: {
+  addStitch: () => void;
   text: string;
 }) {
-  return <TouchableOpacity onPress={()=>addStitch(rowNumber)} className="flex flex-row">
+  return <TouchableOpacity onPress={addStitch} className="flex flex-row">
     <View className="h-8 w-8 bg-gray-400 rounded-full flex flex-row justify-center items-center">
       <Text className="text-white">{text}</Text>
     </View>
@@ -152,11 +199,11 @@ function AddStitchButton({addStitch, rowNumber, text}: {
 function stitchNumberToString(stitchNumber: StitchNumber) {
   switch(stitchNumber) {
     case StitchNumber._1:
-      return "1sc";
+      return "1";
     case StitchNumber._2:
-      return "2sc";
+      return "2";
     case StitchNumber._2tog:
-      return "2tog";
+      return "2t";
     case StitchNumber.nothing:
       return "";
     default:
